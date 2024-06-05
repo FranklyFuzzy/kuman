57d54d04db77d8af2eab12e19b2ed4c30f39aeba
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2024-06-04 21:09:21 -0400

    Delete kuman.sh

--- kuman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "# Config - Kuman 7-inch Touchscreen" >> /boot/config.txt
-echo "hdmi_cvt 800 480 60 6 0 0 0" >> /boot/config.txt
-echo "hdmi_group=2" >> /boot/config.txt
-echo "hdmi_mode=1" >> /boot/config.txt
-echo "hdmi_mode=87" >> /boot/config.txt
-echo "max_usb_current=1" >> /boot/config.txt
-
-sudo reboot
